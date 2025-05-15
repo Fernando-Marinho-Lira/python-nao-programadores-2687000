c2a67145548ad411d60fd017753711c3df8603a1
Fernando-Marinho-Lira <140839294+Fernando-Marinho-Lira@users.noreply.github.com>
2025-05-15 18:16:08 +0000

    NumericoFeito

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,10 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print("Paloma"*2)
 
 # Realize uma operação entre dados do tipo inteiro e float
+f"O resultado da soma dos dados String e inteiro é {altura+idade_numerica}"
