68538831263fed99ab77fad87e0a53b7a38f09de
Fernando-Marinho-Lira <140839294+Fernando-Marinho-Lira@users.noreply.github.com>
2025-05-15 16:06:30 +0000

    Exercicio variaveis mod

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,19 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Fernando'
+profissao = "Analista de Rede"
+objetivo = '''
+... Aprender 
+... Python
+... de forma rápida
+'''
 
 # Declare variáveis de cada tipo numérico
-
+idade = 44
+salario = 3550,00
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+
+falso = False
